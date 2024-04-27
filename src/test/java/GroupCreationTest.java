ed72ebb4f3c76cf73de23d9899d840a80fb8e29c
dmitriytimoshenko73 <dmtimoshenko@mail.ru>
2024-04-27 14:12:35 +0300

    changed  Web_TestHW1

--- src/test/java/GroupCreationTest.java
+++ src/test/java/GroupCreationTest.java
@@ -1,46 +1,57 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.io.File;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.time.Duration;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 public class GroupCreationTest {
-    private WebDriver driver;
+
+    private  WebDriver driver;
+    private WebDriverWait wait;
+    private  static java.lang.String USERNAME;
+    private static java.lang.String PASSWORD;
 
     @BeforeAll
-    public static void setUpClass() {
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/geckodriver");
+    public static void setupClass(){
+        System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\geckodriver");
+        USERNAME = "GB202310790789";
+        PASSWORD = "0a5b2e39a5";
     }
 
     @BeforeEach
-    public void setUp() {
+    public void setupTest(){
         driver = new FirefoxDriver();
-        driver.get("https://test-stand.gb.ru/login");
-        driver.findElement(By.name("username")).sendKeys("GB202310790789");
-        driver.findElement(By.name("password")).sendKeys("0a5b2e39a5");
-        driver.findElement(By.tagName("button")).click();
+        wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+        driver.manage().window().maximize();
     }
 
     @Test
-    public void testGroupCreation() throws IOException {
-        //WebDriverWait wait = new WebDriverWait(driver, 10);
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        // Нажимаем на '+' для добавления группы
-        driver.findElement(By.cssSelector("create-button")).click();
+    void testStandGeekBrains() throws IOException {
+        driver.get("https://test-stand.gb.ru/login");
+        wait.until(ExpectedConditions.presenceOfElementLocated(
+                By.cssSelector("form#login input[type='text']"))).sendKeys(USERNAME);
+        wait.until(ExpectedConditions.presenceOfElementLocated(
+                By.cssSelector("form#login input[type='password']"))).sendKeys(PASSWORD);
 
-        // Вводим имя новой группы
+        WebElement loginButton = driver.findElement(By.cssSelector("form#login button"));
+        loginButton.click();
+        wait.until(ExpectedConditions.invisibilityOf(loginButton));
+
+        WebElement usernameLink = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.partialLinkText(USERNAME)));
+        assertEquals(String.format("Hello, %s", USERNAME), usernameLink.getText().replace("\n", " ").trim());
+
+        WebElement createUser = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("create-btn")));
+        createUser.click();
 
         String firstName = "UserName"+ System.currentTimeMillis();
         String login = "g"+ System.currentTimeMillis();
@@ -51,29 +62,21 @@ public class GroupCreationTest {
         WebElement loginUser = wait.until(ExpectedConditions.visibilityOfElementLocated(By.
                 xpath("//form//span[contains(text(), 'Login')]/following-sibling::input")));
         loginUser.sendKeys(login);
-
-        // Нажимаем кнопку SAVE
-        driver.findElement(By.id("save-button")).click();
+        WebElement buttonSave = driver.findElement(By.cssSelector("form div.submit button"));
+        buttonSave.click();
         driver.navigate().refresh();
-
-        // Проверяем, что группа с именем появилась в таблице
 
         String xpath = String.format("//table[@aria-label='Dummies list']/tbody//td[text()='%s']", firstName);
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
         WebElement newUser = driver.findElement(By.xpath(xpath));
         assertTrue(newUser.getText().contains(firstName));
 
-        // Сохраняем скриншот окна браузера
-        File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        Path screenshotPath = Paths.get("src/test/resources/screenshot.png");
-        Files.copy(screenshot.toPath(), screenshotPath);
+        byte[] screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
+        Files.write(Path.of("src/test/resources/screenshot" + System.currentTimeMillis() + ".png"), screenshot);
     }
 
     @AfterEach
-    public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
+    public void teardown(){
+        driver.quit();
     }
 }
-
